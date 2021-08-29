4d00ec41b0d76e9ba4e82b5a5d13736e5ee29ed2
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-08-29 21:27:04 +0600

    object and function

--- js script/test.js
+++ js script/test.js
@@ -1,2 +1,41 @@
 console.log('Hello ');
 document.write('Bangladesh');
+
+function sleep(name,time){
+    console.log(name + "  is sleeping at " + time);
+}
+console.log('My name is jasim');
+sleep("jasim","10 pm");
+sleep("karim","2 am");
+sleep("rahim","12 am");
+function goodreaturn(a,b){
+    return a*b;
+}
+let x = goodreaturn(4,5);
+console.log(x);
+
+function MyFunction(){
+    let carName = "volvo";
+    console.log(carName);
+}
+MyFunction();
+
+
+//object
+const car = {
+    name:"fiat",
+    model:500,
+    weight:"850 kg",
+    color:"white",
+    start: function(){
+        this.drive();
+        console.log("car has started");
+    },
+    drive: function(){
+        console.log("car is drived");
+    },
+
+}
+console.log(car.weight);
+console.log(car['model']);
+car.start();
