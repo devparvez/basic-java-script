78ae5c31587b4d65f375aa35c3e4aaeb1058b990
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-09-02 12:31:40 +0600

    array iteration

--- js script/test.js
+++ js script/test.js
@@ -1,27 +1,88 @@
-const fruits = ["Apple","mango","banana","pineapples"]
-fruits.push("guava");
-fruits[fruits.length] = "Rice";
-//let flen = fruits.length;
-//console.log(fruits[fruits.length-1]);
-//for(i=0; i<fruits.length; i++){
-  //  console.log(i);
- //   console.log(fruits[i]);
-//}
-//let text ="<ul>";
+const points = [20,39,1,3,53,55];
+points.sort(function(a,b){
+    return a-b;
+});
+console.log(points);
 
-//for(i=0; i < flen; i++){
- //   text = text +  fruits[i]
-//}
-function myFunction(value){
-    console.log(value);
-}
-fruits.forEach(myFunction);
+// array method
+//const numbers = [45,20,5,6,3];
 
-let myGirls = ["asa","bina","disa","nice"];
-let myBoys = ["aman","ayaj","jaman"];
-let a = myGirls.concat(myBoys);
-console.log(a);
-let b=a.concat("peter");
-console.log(b);
-let citrus = myGirls.slice(1,3);
-console.log(citrus);
+
+//function myFunction(value,index,array){
+   // console.log(value);
+   // console.log(index);
+   // console.log(array);
+   // console.log("-------------");
+//}
+
+//maping
+
+//numbers.forEach(myFunction);
+//function myFunction(value,index,array){
+  //  return value*2;
+//}
+
+
+
+
+//const newNumbers = numbers.map(myFunction);
+//console.log(newNumbers);
+
+//filter
+
+
+//function myFunction(value,index,array){
+   // return value > 10;
+//}
+
+
+
+
+//const newNumbers = numbers.filter(myFunction);
+//console.log(newNumbers);
+
+//reduce function
+
+//const numbers = [45,20,5,6,3];
+
+//function myFunction(total,value,index,array){
+  //  return total+ value;
+//}
+
+
+
+
+//const newNumbers = numbers.reduce(myFunction);
+//console.log(newNumbers);
+// array . every
+//const numbers = [45, 4, 9, 16, 25];
+//let allOver18 = numbers.every(myFunction);
+
+//function myFunction(value, index, array) {
+//  return value > 2;
+//}
+//const newNumbers = numbers.reduce(myFunction);
+//console.log(allOver18);
+
+
+//array some
+
+//const numbers = [45, 4, 9, 16, 25];
+//let allOver18 = numbers.some(myFunction);
+
+//function myFunction(value, index, array) {
+ // return value > 25;
+//}
+//console.log(allOver18);
+
+// find array
+
+//const number = [45, 4, 9, 16, 25];
+//let allOver18 = array.from("abcdef");
+
+//function myFunction(value, index, array) {
+ // return value > 5;
+//}
+//console.log(allOver18);
+console.log(Array.from("ABCDEFG")) ; 
+
