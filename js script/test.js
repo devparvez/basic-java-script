a250d65ee0d68e145fd909663299b0c543956cdd
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-09-08 00:04:20 +0600

    Searching and filtering an array

--- js script/test.js
+++ js script/test.js
@@ -1,77 +1,45 @@
-// ----------------Modern javascript----------------------
+//------ Searching and Filtering an array----------
+// -------use indexof for array searching---------
 
-// difference of var, let and const perents theke child e jabe let but child theke parents e jabena etai blockscope
+//let myFriendNames = ["parvez","mahmud","shila","lila","joiana","parvez"];
+//console.log(myFriendNames.indexOf("parvez",0));
 
-// function biodata(){
-//  let myFirstName = "parvez";
-//  console.log(myFirstName);
+//-------- last indexOf -------
+// let myFriendNames = ["parvez","mahmud","shila","lila","joiana","parvez","shila"];
 
-//  if(true){
-//    let myLastName = "mahmud";
-//    console.log("inner "+myFirstName);
-//    console.log("inner "+myLastName);
-//  }
-//  console.log("inner outer "+myLastName);
-// }
-// biodata();
+// console.log(myFriendNames.lastIndexOf("parvez",3));
 
-//-------------------template literal-------------
-//table of (8,9,10) with template literal
+//----------Array.includes------------------------
 
-// for(let num = 1; num <= 20; num++){
-//   let tableof = 8;
-//   console.log(`${tableof} * ${num} = ${tableof*num}`);
-// }
+// let myFriendNames = ["parvez","mahmud","shila","lila","joiana","parvez","shila"];
+// console.log(myFriendNames.includes("parvez"));
 
-// ----------------------Default parameters of functions----
-// function mult(a,b=5){
-//   return a*b;
-// }
-// console.log(mult(3));
-//--------Fat arrow function-----------
+// ---------------array.find------------------------
 
-// const sum = () => {
-//   let a = 5; let b = 6;
-  
-//   console.log(`the sum of 2 numbers are ${a + b}`);
-// }
-// sum();
-
-// one line fat arrow function
-
-// const sum = () => `the sum of two numbers are ${(a = 5)+(b =6)}`;
-// console.log(sum());
-
-//-------- Array-------
-
-//let myFriends = ["parvez","mahmud","sohel","lila"];
-
-//console.log(myFriends.length);
-//console.log(myFriends[myFriends.length-1]);
-//for(i = 0; i < myFriends.length;i++){
- // console.log(myFriends[i]);
-//}
-
-//-------------for in loop and for loop of---------------------
-//let myFriends = ["parvez","mahmud","sohel","lila"];
-// for(let khobis in myFriends){
-//   console.log(khobi);
-// }
-// for(let elements of myFriends){
-//   console.log(elements);
-// }
-
-
-// -----------for each loop----------
-
-// let myFriends = ["parvez","mahmud","sohel","lila"];
-// myFriends.forEach(function(element,index,array){
-//   console.log(element+"index "+index+"array "+array)
+//const prices = [100,200,250,300,400,450,500,600.700];
+// const findElm = prices.find((currval) => {
+//   return currval < 400;
 // })
+// console.log(findElm);
 
-//forEach loop with Fat arrow function
-let myFriends = ["parvez","mahmud","sohel","lila"];
-myFriends.forEach((element,index,array) =>
-console.log(element+"index "+index+"array "+array)
+// alt
+// const findElm = prices.find((currval) =>
+//  currval<400
+// )
+// console.log(findElm);
+//alt
+//console.log(prices.find((currval) =>currval<400));
+//alt
+//console.log(prices.findIndex((currval) => currval < 400));
 
-)
+//-------- filter array----------
+// const prices = [100,200,250,300,400,450,500,600.700];
+
+// let newPriceTag = prices.filter((elem,index) =>
+//  elem < 400
+// )
+// console.log(newPriceTag);
+
+// //----------sorting an array----------------
+// const months = ["march","january","Feb","Dec","Nov"];
+// console.log(months.sort());
