3b1f54d1ca3d4e73b003942969641f407a0b276a
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-09-08 11:41:52 +0600

    crud method (push,pop,shift,unshift,reduce etc)

--- js script/test.js
+++ js script/test.js
@@ -1,45 +1,118 @@
-//------ Searching and Filtering an array----------
-// -------use indexof for array searching---------
+//--------crud(push,pop etc)---------------------------
+//-------push method-----------------
+// const animals = ["pigs","goats","cow","sheep"];
 
-//let myFriendNames = ["parvez","mahmud","shila","lila","joiana","parvez"];
-//console.log(myFriendNames.indexOf("parvez",0));
+// const count = animals.push("Chicken","elephant","tiger");
+// console.log(animals);
+// console.log(count);
 
-//-------- last indexOf -------
-// let myFriendNames = ["parvez","mahmud","shila","lila","joiana","parvez","shila"];
+// ----------unshift method ad first elements------
 
-// console.log(myFriendNames.lastIndexOf("parvez",3));
+// const animals = ["pigs","goats","cow","sheep"];
+// let count = animals.unshift("Cow");
+// console.log(animals);
+// console.log(count);
+// 2nd example for numbers unshift method
 
-//----------Array.includes------------------------
+// const numbers = [1,2,5];
+// let count = numbers.unshift(3,4);
+// console.log(numbers);
+// console.log(count);
 
-// let myFriendNames = ["parvez","mahmud","shila","lila","joiana","parvez","shila"];
-// console.log(myFriendNames.includes("parvez"));
+// ------pop method--------
 
-// ---------------array.find------------------------
+// let plants = ["broccoli","cauliflower","kale","tomato","cabbage"];
+// console.log(plants);
+// console.log(plants.pop());
+// console.log(plants);
 
-//const prices = [100,200,250,300,400,450,500,600.700];
-// const findElm = prices.find((currval) => {
-//   return currval < 400;
+// -------shift method---------------
+// plants = ["broccoli","cauliflower","kale","tomato","cabbage"];
+// console.log(plants);
+// console.log(plants.shift());
+// console.log(plants);
+
+//-------- splice method---------
+//sol-1
+// const months = ["jan","march","april","jun","jul"];
+ //const newMonths = months.splice(months.length,0,"Dec");
+// console.log(months); 
+
+//sol-2
+//console.log(newMonths);
+//sol -3 replace march with March 
+//const months = ["jan","march","april","jun","jul"];
+//let indexOfMonth = months.indexOf("april");
+//if(indexOfMonth != -1){
+//  console.log(months.splice(indexOfMonth,1,"April"))
+ // console.log(months);
+//}else{
+ // console.log("Please enter valid data");
+//}
+//const newMonths = months.splice(1,1,"March");
+//console.log(months);
+
+// sol4 delete june
+// const months = ["jan","march","april","jun","jul"];
+// let indexOfMonth = months.indexOf("jun");
+// if(indexOfMonth != -1){
+//   console.log(months.splice(indexOfMonth,1))
+//   console.log(months);
+// }else{
+//   console.log("Please enter valid data");
+// }
+// const array1 = [1,4,9,16,25];
+// const newArr = array1.map((currval,elem,arr) => {
+//   return currval > 9;
+// }) 
+// console.log(array1);
+// console.log(newArr);
+// const newArray = array1.map((currElm,index,arr) => {
+//   return `current elements ${currElm} and index ${index} and array is ${arr}`;
 // })
-// console.log(findElm);
+// console.log(newArray);
 
-// alt
-// const findElm = prices.find((currval) =>
-//  currval<400
-// )
-// console.log(findElm);
-//alt
-//console.log(prices.find((currval) =>currval<400));
-//alt
-//console.log(prices.findIndex((currval) => currval < 400));
+// Find the square root of an array?
+// let arr = [25,36,49,64,81];
+// let newsqrt = arr.map((currElm) => {
+//   return Math.sqrt(currElm);
+// })
+// console.log(newsqrt);
 
-//-------- filter array----------
-// const prices = [100,200,250,300,400,450,500,600.700];
+// sol Multiply all elements by 2 and return those are greater than 10;
+//  let arr = [2,3,6,8];
+//  let newArr = arr.map((curElm) => {
+//    return curElm * 2;
+// }).filter((curElm) => {
+//   return curElm > 10;
+//  }).reduce((total,curElm,index,arra) => {
+//  return total += curElm;
+// })
+//  console.log(newArr);
 
-// let newPriceTag = prices.filter((elem,index) =>
-//  elem < 400
-// )
-// console.log(newPriceTag);
+// -------shorter version--------------
+// let arr = [2,3,6,8];
+// let newArr = arr.map((curElm) => curElm * 2).filter((curElm) =>  curElm > 10)
+// console.log(newArr);
 
-// //----------sorting an array----------------
-// const months = ["march","january","Feb","Dec","Nov"];
-// console.log(months.sort());
+//----------reduce method---------------
+
+//  let arr = [2,5,6];
+
+// let newArr = arr.reduce((total,curElm,index,arra) => {
+//  return total *= curElm;
+// },7)
+//  console.log(newArr);
+
+//-------- flat array-------------------
+const arr = [['zone_1', 'zone_2'],
+             ['zone_3', 'zone_4'],
+             ['zone_5', 'zone_6'],
+             ['zone_7', 'zone_8'],
+             ['zone_9', 'zone_10'],
+];
+
+const newArray = arr.reduce((acum,currval) =>{
+  return acum.concat(currval);
+})
+console.log(newArray);
