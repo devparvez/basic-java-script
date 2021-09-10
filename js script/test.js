c6d787ea419099da3a48a126a0db48f75554a8d8
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-09-10 12:58:15 +0600

    Dom and Bom method

--- js script/test.js
+++ js script/test.js
@@ -1,44 +1,26 @@
-//----------------- Math object in java script------------------
-// value of PI
-//console.log(Math.PI);
+// -------------Dom and Bom method-------------------
+// function myffunction(){
+//     window.history.back();
+// }
+//------ if you want to visit another site-------
+// alert(location.href);
+// if(confirm("want to visit the devparvez?")){
+//     location.href="https://devparvez.com";
+// }
 
-//--------- Round the number -----------
-//let num = 10.234232;
-//console.log(Math.round(num));
-//---------- power the numbers--------------
-//console.log(Math.pow(10,3));
-//console.log(10**3);
+// const changeContent = () => {
+//     document.getElementById("heading").innerHTML = "welcome to parvez's world";
+// }
 
-//------ Square root -------
-
-//console.log(Math.sqrt(25)); 
-//console.log(Math.sqrt(36)); 
-//console.log(Math.sqrt(81)); 
-//console.log(Math.sqrt(66)); 
-
-// --------------- Math.abs------
-// console.log(Math.abs(-4.55));
-// console.log(Math.abs(-5));
-// console.log(Math.abs(-890));
-// console.log(Math.abs(4-6));
-//---math.ceil------
-// console.log(Math.ceil(4.5));
-// console.log(Math.ceil(9.01));
-//----Math.floor-------------
-// console.log(Math.floor(9.01));
-//console.log(Math.floor(9.6));
-
-// -----math.min and math.max======
-
-//console.log(Math.min(2,4,5,-5,4,5,-7));
-//console.log(Math.max(2,4,5,-5,4,5,-7));
-
-//-------- math.random--------------
-console.log(Math.floor(Math.random() * 10));
-
-// Math trunc method-------------
-
-//console.log(Math.trunc(3.5));
-//console.log(Math.trunc(-3.5));
-//console.log(Math.trunc(9.1));
-//console.log(Math.trunc(11.5));
+const change = () =>{
+    //document.getElementById("heading2").innerHTML = "yes ///it is very awesome change";
+//     const headchange = document.getElementById("heading2");
+//     headchange.innerHTML = "this is parvez";
+//     console.log(document.getElementsByClassName("para"));
+//     console.log(document.getElementsByTagName("p"));
+//     console.log(document.getElementsByName("gender"));
+        document.querySelector("#heading2").innerHTML = "I change again";
+        console.log(document.querySelectorAll(".para"));
+ }
+// const headchange = document.getElementById("heading2");
+// headchange.innerHTML = "this is parvez";
