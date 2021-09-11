b6ac7594b7f38fa8b756ca85c404f68fb64a4a7c
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-09-12 02:47:00 +0600

    Events in javascript

--- js script/test.js
+++ js script/test.js
@@ -1,26 +1,89 @@
-// -------------Dom and Bom method-------------------
-// function myffunction(){
-//     window.history.back();
-// }
-//------ if you want to visit another site-------
-// alert(location.href);
-// if(confirm("want to visit the devparvez?")){
-//     location.href="https://devparvez.com";
+//----------------- Events in javascript------------------ 
+//---------2nd way of event listning-------------
+// const callingFunction = () => {
+//     alert('I am function,some body uses me');
+// };
+// //3rd way and it will overwrite using multiple time
+
+// const threeBtn = document.getElementById("thirdWay");
+// threeBtn.onclick = function(){
+//     alert("this is 3rd popular way ");
+// };
+
+// threeBtn.onclick = function(){
+//     console.log("this is 3rd popular way ");
+// };
+// //4th way it will not over write using 4th way
+// const  fourthWay = document.querySelector('#fourthway');
+// fourthWay.addEventListener('click',()=>{
+//     alert("this is fourth way which is add event listn")
+// });
+
+// fourthWay.addEventListener('click',()=>{
+//     console.log("this is fourth way which is add event listn")
+// })
+
+//------- Event ovject------------- 
+
+// const forthWay = document.getElementById("fourthway");
+// const checkEvent = () =>{
+//     alert('I am fourth way alerm');
+//     console.log(event);
+//     console.log(event.target);
+//     console.log(event.type);
+    
 // }
 
-// const changeContent = () => {
-//     document.getElementById("heading").innerHTML = "welcome to parvez's world";
+
+
+// forthWay.addEventListener('click',checkEvent);
+
+//---------------Mouse events----------------------
+//---mouse up and mouse down------------------------
+// function mousedown(){
+//     document.getElementById("mypara").style.color = "red";
+// }
+// function mouseup(){
+//     document.getElementById("mypara").style.color = "orange";
+// }
+// ---------------Mouseenter and Mouse leave---------------
+// const mEnter= document.getElementById("box");
+// mEnter.addEventListener('mouseenter',()=>{
+//     mEnter.style.backgroundColor = "green";
+//     console.log("mouse is entered");
+// });
+// mEnter.addEventListener('mouseleave',()=>{
+//     mEnter.style.backgroundColor = "pink";
+//     console.log("mouse is leaved bro");
+// })
+
+
+//--------------keyboard event objects in javascript-------------
+
+// const keypress = () =>{
+//     document.getElementById("keys").innerHTML = `You press ${event.key} and it's code is ${event.code}`;
 // }
 
-const change = () =>{
-    //document.getElementById("heading2").innerHTML = "yes ///it is very awesome change";
-//     const headchange = document.getElementById("heading2");
-//     headchange.innerHTML = "this is parvez";
-//     console.log(document.getElementsByClassName("para"));
-//     console.log(document.getElementsByTagName("p"));
-//     console.log(document.getElementsByName("gender"));
-        document.querySelector("#heading2").innerHTML = "I change again";
-        console.log(document.querySelectorAll(".para"));
- }
-// const headchange = document.getElementById("heading2");
-// headchange.innerHTML = "this is parvez";
+// const keyup = () =>{
+//     document.getElementById("keys").innerHTML = "This is key up";
+// }
+// const keydown = () =>{
+//     document.getElementById("keys").innerHTML = "this is key Down";
+// }
+
+//-----------------onchange event-----------------------
+
+// const selectElement = () => {
+//     const getText = document.getElementById("ice").value;
+//     const getchange = document.getElementById("icecreames").value;
+//     console.log(`${getText} and ${getchange}`);
+//     const getEvery = document.getElementById("getevery").innerHTML = ` Mr ${getText} and ${getchange} ice cream flavour`;
+// }
+// const icecreames = document.getElementById("icecreames");
+// icecreames.addEventListener("change",()=>{
+//         const getText = document.getElementById("ice").value;
+//    const getchange = document.getElementById("icecreames").value;
+//   console.log(`${getText} and ${getchange}`);
+//   const getEvery = document.getElementById("getevery");
+//   getEvery.innerHTML = ` Mr ${getText} and ${getchange} ice cream flavour`;
+// });
