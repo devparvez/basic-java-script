f3d383e64075fddc8e6f82077133e362344f615b
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-09-13 23:05:39 +0600

    Capturing event ,Bubble Event and calculator of(call back function and higer order functon)

--- js script/test.js
+++ js script/test.js
@@ -1,54 +1,44 @@
-//-------------Es8 features 2017------------
-//------string padding--------------
-// let message = "parvez".padStart(40);
-// console.log(message);
-// let myAge = "27".padEnd(20);
-// console.log(myAge);
+// const parentId = document.getElementById("parentid");
+// const childId = document.getElementById("childid");
 
-//---object.values and object.entries======
+// let parentCall=()=>{
+//   alert('parent div call');
 
-// let person = {
-//   name : "frd",
-//   age : 45
 // };
-// console.log(Object.values(person));
-// console.log(Object.entries(person));
+// let childCall=()=>{
+//   alert('child div call');
+//   //event.stopPropagation();
 
-// ------Es18--------------------
-// const person = {name:"fred",age:87};
-// const sperson = {ella:"sawon",...person};
-// console.log(person);
-// console.log(sperson);
+// }
 
-//--------Flat array-------------
+// // in capturing event we use 3rd parameter true ------------
+// //and by default it is false and called capturing phase------
+// parentId.addEventListener('click',parentCall,true);
+// childId.addEventListener('click',childCall,true);
 
-// const arr =[
-//   ["Zone1", "zone2"],
-//   ["Zone3", "zone4"],
-//   ["Zone5", "zone6"],
-//   ["Zone7", "zone8"],
-//   ["Zone9", ["zone10","zone11"],["zone12","zone13"],["zone12","zone14"]]
-// ];
-// console.log(arr.flat(Infinity));
+//----- Create a calculator -----------
 
-// ------fromentries used--------
-// const person = {
-//   Name:"fred",
-//   age:33
-// };
-// const arrobj = Object.entries(person);
-// console.log(arrobj);
-// console.log(Object.fromEntries(arrobj));
+// Higher order function and call back function;
+// const add = (a,b) =>{
+//   return a+b;
+// }
+// const subs = (a,b) => {
+//   return Math.abs(a-b);
+// }
+// const mult =(a,b) =>{
+//   return a*b;
+// }
+// const calculator = (num1,num2,operator) =>{
+//   return operator(num1,num2);
+// }
 
-//---------- BigInt----------------
-// let oldNum = Number.MAX_SAFE_INTEGER;
-// console.log(oldNum);
-// const newNum = 9007199254740991n+12n;
-// console.log(typeof newNum);
-// console.log(newNum) 
+// console.log(calculator(5,2,subs));
 
-//---Ecmascript use Strict moode
-//------
-// "use strict";
-// let x = 3.14;
-// console.log(x);
+const add = (a,b)=>{
+  return a+b;
+}
+
+const calculator = (num1,num2,operator)=>{
+  return operator(num1,num2);
+}
+console.log(calculator(3,4,add));
